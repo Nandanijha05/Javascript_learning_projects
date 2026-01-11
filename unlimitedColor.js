4e2794f7c5844fe2d98b3b195cd3076b0163b385
Nandani Kumari <nandanijha794@gmail.com>
2026-01-11 22:49:03 +0530

    Initial commit: add background color changer project

---
+++ unlimitedColor.js
@@ -0,0 +1,27 @@
+var start=document.querySelector("#start")
+var stop=document.querySelector("#stop")
+
+var changeColor=function(){
+    document.body.style.backgroundColor=randomColor();
+
+
+}
+let interval;
+function StartChangeColor (){
+    interval=setInterval(changeColor,200);
+}
+var randomColor = function(){
+    var textColor='0123456789abcdef';
+    let randomNum="#";
+    for(let i=0;i<6;i++)
+    {
+        randomNum=randomNum+textColor.charAt(Math.floor(Math.random()*textColor.length))
+    }
+    return randomNum;
+}
+var StopChangeColor=function(){
+    clearInterval(interval);
+    interval=null;
+}
+start.addEventListener("click",StartChangeColor)
+stop.addEventListener("click",StopChangeColor)
